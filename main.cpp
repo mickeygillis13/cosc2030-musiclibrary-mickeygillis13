a964e7531763cdccb1673d25d71b58db0d5d4fb7
mickeygillis13 <mickeygilllis@sheridan.edu>
2026-09-11 02:57:02 +0000

    finished addTrack

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+#include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
+
+struct MusicTrack{
+    char title[100];
+    char album[100];
+    char artist[100];
+    int duration;
+    char genre[100];
+    int releaseYear;
+};
 
 // Collects details for a new music track from user input and adds it to the library.
 void addTrack(vector<MusicTrack>& library);
@@ -18,5 +29,32 @@ void loadLibraryFromFile(vector<MusicTrack>& library, const string& filename);
 void printMenu();
 
 int main() {
-    
+
+}
+
+void addTrack(vector<MusicTrack>& library){
+    MusicTrack newTrack;
+
+    cout << "title of track:" << endl;
+    cin.getline(newTrack.title, 100);
+
+    cout << "What album is the track from?:" << endl;
+    cin.getline(newTrack.album, 100);
+
+    cout << "Artist of track:" << endl;
+    cin.getline(newTrack.artist, 100);
+
+    cout << "Duration of track:" << endl;
+    cin >> newTrack.duration;
+    cin.ignore();
+
+    cout << "Genre of track:" << endl;
+    cin.getline(newTrack.genre, 100);
+
+    cout << "Release year of track:" << endl;
+    cin >> newTrack.releaseYear;
+    cin.ignore();
+
+    library.push_back(newTrack);
+
 }
